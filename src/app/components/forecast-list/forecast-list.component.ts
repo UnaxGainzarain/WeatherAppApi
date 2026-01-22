bda349f626bca7f8a0dd320adbb826ca0a7f5dd2
UnaxGainzarain <unaxarbizu1@gmail.com>
2026-01-22 14:11:52 +0100

    feat: implementado lista forecast de 5 siguientes días

--- src/app/components/forecast-list/forecast-list.component.ts
+++ src/app/components/forecast-list/forecast-list.component.ts
@@ -1,14 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonList, IonItem, IonLabel, IonNote, IonIcon, IonText } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-forecast-list',
   templateUrl: './forecast-list.component.html',
   styleUrls: ['./forecast-list.component.scss'],
+  standalone: true,
+  imports: [CommonModule, IonList, IonItem, IonLabel, IonNote, IonIcon, IonText]
 })
-export class ForecastListComponent  implements OnInit {
+export class ForecastListComponent implements OnInit {
+  @Input() items: any[] = [];
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() { }
+
 
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { WeatherCardComponent } from '../components/weather-card/weather-card.component';
+import { ForecastListComponent } from '../components/forecast-list/forecast-list.component';
 import { WeatherService } from '../services/weather.service';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonSearchbar, IonButtons, IonButton, IonIcon } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
@@ -11,11 +12,12 @@ import { locate } from 'ionicons/icons';
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [CommonModule, IonHeader, IonToolbar, IonTitle, IonContent, WeatherCardComponent, FormsModule, IonSearchbar, IonButtons, IonButton, IonIcon],
+  imports: [CommonModule, IonHeader, IonToolbar, IonTitle, IonContent, WeatherCardComponent, ForecastListComponent, FormsModule, IonSearchbar, IonButtons, IonButton, IonIcon],
 })
 export class HomePage {
   searchTerm: string = '';
   weatherData: any;
+  forecastList: any[] = [];
 
   constructor(private weatherService: WeatherService) {
     addIcons({ locate });
@@ -31,6 +33,16 @@ export class HomePage {
       },
       error: (err) => {
         console.error('Error fetching weather:', err);
+      }
+    });
+
+    this.weatherService.getForecastByCity(this.searchTerm).subscribe({
+      next: (data) => {
+        this.forecastList = data.list; // OpenWeather returns list in 'list' property
+        console.log('Forecast data:', data);
+      },
+      error: (err) => {
+        console.error('Error fetching forecast:', err);
       }
     });
   }
