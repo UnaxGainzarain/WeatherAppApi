448ca364f7d6e04a7665059d9c81c923126bad3e
UnaxGainzarain <unaxarbizu1@gmail.com>
2026-01-22 13:30:37 +0100

    feat: añadido barra de búsqueda y geolocalización

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { WeatherCardComponent } from '../components/weather-card/weather-card.component';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonSearchbar, IonButtons, IonButton, IonIcon } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { locate } from 'ionicons/icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, WeatherCardComponent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, WeatherCardComponent, FormsModule, IonSearchbar, IonButtons, IonButton, IonIcon],
 })
 export class HomePage {
-  constructor() { }
+  searchTerm: string = '';
+
+  constructor() {
+    addIcons({ locate });
+  }
+
+  searchCity() {
+    console.log('Searching for:', this.searchTerm);
+    // TODO: Connect to service
+  }
+
+  getCurrentLocation() {
+    console.log('Getting current location...');
+    // TODO: Implement Gelocation logic
+  }
 }
