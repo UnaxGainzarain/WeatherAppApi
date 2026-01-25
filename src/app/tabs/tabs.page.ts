57aa2aae87f143c0ecbe47592b78f5570f78cc63
UnaxGainzarain <unaxarbizu1@gmail.com>
2026-01-25 18:34:24 +0100

    style: cambiados los tabs de por días y por horas se cambien según el idioma

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { time, calendar } from 'ionicons/icons';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
     selector: 'app-tabs',
     templateUrl: 'tabs.page.html',
     styleUrls: ['tabs.page.scss'],
     standalone: true,
-    imports: [IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel],
+    imports: [IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel, TranslateModule],
 })
 export class TabsPage {
 
